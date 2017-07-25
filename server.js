b0c0f7eb97d18963bacd240fcec3b738e0f5b324
langkilde <daniel.langkilde@gmail.com>
2017-07-25 09:25:19 +0200

    some cleanup

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-const express = require("express");
-const logger = require('morgan');
-const path = require('path');
-const https = require('https');
-const fs = require('fs');
-const server = express();
-
+var express = require("express");
+var logger = require('morgan');
+var path = require('path');
+var https = require('https');
+var fs = require('fs');
+var server = express();
 server.use(logger('combined'));
-
 if (process.env.NODE_ENV !== 'production') {
-  console.log('starting dev server');
-  const webpackDevMiddleware = require("webpackdevmiddleware");
-  const webpack = require("webpack");
-  const webpackConfig = require("./webpack.config.js");
-  server.use(webpackDevMiddleware(webpack(webpackConfig)));
-  server.use(webpackDevMiddleware(webpack(webpackConfig)));
-} else {
-  console.log('starting production server');
-  server.use(express.static('build'));
-  server.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'build/index.html'));
+    console.log('starting dev server');
+    var webpackDevMiddleware = require("webpackdevmiddleware");
+    var webpack = require("webpack");
+    var webpackConfig = require("./webpack.config.js");
+    server.use(webpackDevMiddleware(webpack(webpackConfig)));
+    server.use(webpackDevMiddleware(webpack(webpackConfig)));
+}
+else {
+    console.log('starting production server');
+    server.use(express.static('build'));
+    server.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build/index.html'));
     });
 }
-
-const PORT = 8080;
-server.listen(PORT, () => console.log('listening to port', PORT));
+var PORT = 8080;
+server.listen(PORT, function () { return console.log('listening to port', PORT); });
---
+++ server.ts
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require('morgan');
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
+const server = express();
+
+server.use(logger('combined'));
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('starting dev server');
+  const webpackDevMiddleware = require("webpackdevmiddleware");
+  const webpack = require("webpack");
+  const webpackConfig = require("./webpack.config.js");
+  server.use(webpackDevMiddleware(webpack(webpackConfig)));
+  server.use(webpackDevMiddleware(webpack(webpackConfig)));
+} else {
+  console.log('starting production server');
+  server.use(express.static('build'));
+  server.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'build/index.html'));
+    });
+}
+
+const PORT = 8080;
+server.listen(PORT, () => console.log('listening to port', PORT));
